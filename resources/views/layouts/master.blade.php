6c46032674f910ef904216abfb37e0ab7eea8e37
Joe Gibbs <joe.gibbs0@gmail.com>
2018-09-04 16:49:29 +1000

    Added layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+  @include('layouts.head')
+  <body>
+    @include('layouts.header')
+    @yield('content')
+    @include('layouts.footer')
+  </body>
+</html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+@section('page-title', 'Welcome to Unda')
+
+@section('content')
+
+@endsection
