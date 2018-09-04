68953f67b382864c5112b05e59ca5254af8c65b7
Joe Gibbs <joe.gibbs0@gmail.com>
2018-09-04 19:18:36 +1000

    Removed unused functions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return view('posts.index', ['posts', $posts]);
     }
 
     /**
@@ -52,29 +52,6 @@ class PostController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(StorePost $request, $id)
-    {
-        //
-    }
-
-    /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
@@ -82,6 +59,7 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+      flash()->success('Post deleted!');
+      return Redirect::to('/');
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -79,6 +79,7 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+      flash()->success('Account deleted!');
+      return Redirect::to('/');
     }
 }
