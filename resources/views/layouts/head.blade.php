5614fe37e96a4a224b212696d05d3ef93cbc917e
Joe Gibbs <joe.gibbs0@gmail.com>
2018-09-04 16:52:07 +1000

    Added favicon for all sites

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -2,7 +2,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="apple-touch-icon" sizes="180x180" href="{{URL::to('apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{URL::to('favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{URL::to('favicon-16x16.png')}}">
+    <link rel="manifest" href="{{URL::to('site.webmanifest')}}">
+    <link rel="mask-icon" href="{{URL::to('safari-pinned-tab.svg')}}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#191919">
+    <meta name="theme-color" content="#ffffff">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
