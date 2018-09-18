e5cf04d6f46696ba9ba8c8f3d848c3209a0953ca
Joe Gibbs <joe.gibbs0@gmail.com>
2018-09-18 17:35:33 +1000

    Added main page info and show for posts

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,4 +3,66 @@
 
 @section('content')
 
+  <div class="topmenu">
+		<div class="search">
+			<input type="text" id="searchInput" placeholder="Search Unda"/>
+			<div class="searchIcon">
+				<img src="http://icons.iconarchive.com/icons/icons8/ios7/256/Very-Basic-Search-icon.png"></img>
+			</div>
+		</div>
+	</div>
+
+		<aside class="column-left">
+			<header>
+				<h1><a href="#">Unda</a></h1>
+				<h2>Welcome to Unda, the home of video social media!</h2>
+			</header>
+			<nav id="mainnav">
+				<ul>
+	            	<li class="selected-item"><a href="index.html">Home</a></li>
+	           		<li><a href="#">My account</a></li>
+	           		<li><a href="#">Upload</a></li>
+	            	<li><a href="#">Friends</a></li>
+	            	<li><a href="#">Settings</a></li>
+	        	</ul>
+			</nav>
+		</aside>
+
+		<section class="column-middle">
+		   <div class="post">
+			    <video width="665" height="480" controls></video>
+				    <section id="interact_bar">
+					    <a class="button" id='b_love'>Love</a>
+					    <a class="button" id='b_hate'>Hate</a>
+					    <a class="button" id='b_comment'>Comment</a>
+					    <a class="button" id='b_share'>Share</a>
+					</section>
+			</div>
+
+			<div class="post">
+			    <video width="665" height="432" controls></video>
+				    <section id="interact_bar">
+					    <a class="button" id='b_love'>Love</a>
+					    <a class="button" id='b_hate'>Hate</a>
+					    <a class="button" id='b_comment'>Comment</a>
+					    <a class="button" id='b_share'>Share</a>
+					</section>
+			</div>
+
+			<div class="post">
+			    <video width="665" height="432" controls></video>
+				    <section id="interact_bar">
+					    <a class="button" id='b_love'>Love</a>
+					    <a class="button" id='b_hate'>Hate</a>
+					    <a class="button" id='b_comment'>Comment</a>
+					    <a class="button" id='b_share'>Share</a>
+					</section>
+			</div>
+
+		</section>
+
+		<section class="column-right">
+		Test text.. Not sure what would go here.. Ads? Stories? Dunno.
+		</section>
+
 @endsection
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,9 @@
+<div class="post">
+    <video width="576" height="432" controls></video>
+    <section id="interact_bar">
+	    <a class="button" id='b_love'>Love</a>
+	    <a class="button" id='b_hate'>Hate</a>
+	    <a class="button" id='b_comment'>Comment</a>
+	    <a class="button" id='b_share'>Share</a>
+	</section>
+</div>
