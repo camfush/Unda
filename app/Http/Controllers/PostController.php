6ae4b3430fe06ca3bdf31d92c800d4cd332194c7
Joe Gibbs <joe.gibbs0@gmail.com>
2018-09-18 17:46:02 +1000

    Added show functionality to post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -49,6 +49,8 @@ class PostController extends Controller
      */
     public function show($id)
     {
+      $post = Post::find($id);
+      return view('posts.show', ['post', $post]);
     }
 
     /**
