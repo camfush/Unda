db3546ff51c6d4750c47b09c14ac2f2325736b07
Joe Gibbs <joe.gibbs0@gmail.com>
2018-09-20 14:53:35 +1000

    Added auth::check to see if logged in to show user information

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,68 +14,72 @@
         </form>
     </div>
     <div class="header-block header-block-nav">
+      @if (Auth::check())
         <ul class="nav-profile">
-            <li class="notifications new">
-                <a href="" data-toggle="dropdown">
-                    <i class="fa fa-bell-o"></i>
-                    <sup>
-                        <span class="counter">8</span>
-                    </sup>
-                </a>
-                <div class="dropdown-menu notifications-dropdown-menu">
-                    <ul class="notifications-container">
-                        <li>
-                            <a href="" class="notification-item">
-                                <div class="img-col">
-                                    <div class="img" style="background-image: url('assets/faces/1.png')"></div>
-                                </div>
-                                <div class="body-col">
-                                    <p>
-                                        <span class="accent">Jacob Lewis</span> uploaded new video:
-                                        <span class="accent">Testing Unda out</span>. </p>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="" class="notification-item">
-                                <div class="img-col">
-                                    <div class="img" style="background-image: url('assets/faces/1.png')"></div>
-                                </div>
-                                <div class="body-col">
-                                    <p>
-                                        <span class="accent">Jacob Lewis</span> commented on your video:
-                                        <span class="accent">Blah blah blah</span>. </p>
-                                </div>
-                            </a>
-                        </li>
+          <li class="notifications new">
+            <a href="" data-toggle="dropdown">
+              <i class="fa fa-bell-o"></i>
+              <sup>
+                <span class="counter">8</span>
+              </sup>
+            </a>
+            <div class="dropdown-menu notifications-dropdown-menu">
+              <ul class="notifications-container">
+                <li>
+                  <a href="" class="notification-item">
+                    <div class="img-col">
+                      <div class="img" style="background-image: url('assets/faces/1.png')"></div>
+                    </div>
+                    <div class="body-col">
+                      <p>
+                        <span class="accent">Jacob Lewis</span> uploaded new video:
+                        <span class="accent">Testing Unda out</span>. </p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="" class="notification-item">
+                      <div class="img-col">
+                        <div class="img" style="background-image: url('assets/faces/1.png')"></div>
+                      </div>
+                      <div class="body-col">
+                        <p>
+                          <span class="accent">Jacob Lewis</span> commented on your video:
+                          <span class="accent">Blah blah blah</span>. </p>
+                        </div>
+                      </a>
+                    </li>
 
+                  </ul>
+                  <footer>
+                    <ul>
+                      <li>
+                        <a href=""> View All </a>
+                      </li>
                     </ul>
-                    <footer>
-                        <ul>
-                            <li>
-                                <a href=""> View All </a>
-                            </li>
-                        </ul>
-                    </footer>
+                  </footer>
                 </div>
-            </li>
-            <li class="profile dropdown">
+              </li>
+              <li class="profile dropdown">
                 <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                    <div class="img" style="background-image: url({{URL::to('images/faces/1.png')}})"> </div>
-                    <span class="name"> Jacob Lewis </span>
+                  <div class="img" style="background-image: url({{URL::to('images/faces/1.png')}})"> </div>
+                  <span class="name"> Jacob Lewis </span>
                 </a>
                 <div class="dropdown-menu profile-dropdown-menu" aria-labelledby="dropdownMenu1">
+                  <a class="dropdown-item" href="#">
+                    <i class="fa fa-user icon"></i> Profile </a>
                     <a class="dropdown-item" href="#">
-                        <i class="fa fa-user icon"></i> Profile </a>
-                    <a class="dropdown-item" href="#">
-                        <i class="fa fa-bell icon"></i> Notifications </a>
-                    <a class="dropdown-item" href="#">
+                      <i class="fa fa-bell icon"></i> Notifications </a>
+                      <a class="dropdown-item" href="#">
                         <i class="fa fa-gear icon"></i> Settings </a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="login.html">
-                        <i class="fa fa-power-off icon"></i> Logout </a>
-                </div>
-            </li>
-        </ul>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="login.html">
+                          <i class="fa fa-power-off icon"></i> Logout </a>
+                        </div>
+                      </li>
+          </ul>
+            @else
+              login and signup here
+            @endif
     </div>
 </header>
