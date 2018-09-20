670e8001eef9af221d8aee6c0cc1233779814b48
Joe Gibbs <joe.gibbs0@gmail.com>
2018-09-20 15:42:19 +1000

    Added 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+@section('page-title', 'Welcome to Unda')
+
+@section('content')
+    <div class="">
+      <h2>The page you are looking for could not be found.</h2>
+    </div>
+  @endsection
