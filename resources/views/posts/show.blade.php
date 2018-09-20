e3efd6ca041875e4e115f302fb2b7e745a7befce
Joe Gibbs <joe.gibbs0@gmail.com>
2018-09-20 16:05:27 +1000

    Moved posts to posts.show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,20 @@
-<div class="post">
-    <video width="576" height="432" controls></video>
-    <section id="interact_bar">
-	    <a class="button" id='b_love'>Love</a>
-	    <a class="button" id='b_hate'>Hate</a>
-	    <a class="button" id='b_comment'>Comment</a>
-	    <a class="button" id='b_share'>Share</a>
-	</section>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-9" id="video">
+      <video controls><source src="" type="video/mp4"/></video>
+
+    </div>
+    <div class="col-lg-3 comments" style="background-color: #d7dde4">
+      <p><h3>Comments</h3></p>
+
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-9 m-1 interact-bar">
+      <button type="button" class="btn custom-button">Love</button>
+      <button type="button" class="btn custom-button">Hate</button>
+      <button type="button" class="btn custom-button">Share</button>
+      <button type="button" class="btn custom-button">Report</button>
+    </div>
+  </div>
 </div>
