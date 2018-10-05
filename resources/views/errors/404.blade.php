051ea71edca10d851e5f145a45e08c27977601fa
Joe Gibbs <joe.gibbs0@gmail.com>
2018-10-05 18:38:18 +1000

    Fixed 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,8 +1,21 @@
 @extends('layouts.master')
-@section('page-title', 'Welcome to Unda')
+@section('page-title', 'Error 404: Page not found')
 
 @section('content')
-    <div class="">
-      <h2>The page you are looking for could not be found.</h2>
-    </div>
+    <article class="content dashboard-page">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-12">
+
+            <div class="">
+              <h2>The page you are looking for could not be found.</h2>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+
+        </div>
+      </div>
+    </article>
+
   @endsection
