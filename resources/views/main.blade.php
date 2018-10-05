1360c1a44412ecee6180a669cbcc2df11c5f6dbd
Joe Gibbs <joe.gibbs0@gmail.com>
2018-10-05 20:19:01 +1000

    Fixed up main page and show posts

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,10 +3,6 @@
 
 @section('content')
     <article class="content dashboard-page">
-      @for ($i=0; $i < 10; $i++)
-        @include('posts.show')
-      @endfor
-
       {{-- Show the user's timeline
       @foreach (Auth::user()->timeline as $post)
         @include('posts.show', ['element' => $post])
