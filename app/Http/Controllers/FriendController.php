471619e84a990e8d62148193ba2b7599046251f2
Joe Gibbs <joe.gibbs0@gmail.com>
2018-10-07 09:45:26 +1100

    Added friend requests

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -4,9 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Friend;
+use App\FriendRequest;
 
 class FriendController extends Controller
 {
+
+    public function sendRequest($from, $to)
+    {
+      $fRequest = FriendRequest::create([
+        'from' => $from->id,
+        'to' => $to->id,
+      ]);
+      return $fRequest;
+    }
+
+    public function acceptRequest($fRequest)
+    {
+      $friend = Friend::create([
+        'friend1' => $fRequest->from,
+        'friend2' => $fRequest->to,
+      ]);
+      $fRequest->delete();
+      return $friend;
+    }
     /**
      * Display a listing of the resource.
      *
