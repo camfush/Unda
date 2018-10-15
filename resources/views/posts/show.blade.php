c8d17dbf1fd8e1bb4f494996e6b5a718df46e01d
CamFush <cjfrost28@gmail.com>
2018-10-15 21:47:54 +1100

    Changed sizes

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,9 @@
 <div class="container-fluid">
   <div class="row">
-    <div class="col-lg-9" id="video">
+    <div class="col-lg-8" id="video">
       <video controls><source src="{{Storage::url('posts/' . $element->hash)}}" type="video/mp4"/></video>
     </div>
-    <div class="col-lg-3 comments" style="background-color: #d7dde4">
+    <div class="col-lg-4 comments" style="background-color: #d7dde4">
       <p><h3>Comments</h3></p>
       @if(count($element->comments) > 0)
       <table>
