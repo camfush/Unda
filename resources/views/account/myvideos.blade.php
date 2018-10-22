28fd468342e4e9dcfc8410d4e722bd24b20b357e
Joe Gibbs <joe.gibbs0@gmail.com>
2018-10-23 09:48:53 +1100

    Added br to myvideos

--- resources/views/account/myvideos.blade.php
+++ resources/views/account/myvideos.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.master')
 @section('page-title', 'View your details')
 @section('content')
+  <br style="height:100px;">
   <p>{{count(Auth::user()->posts)}}</p>
   @foreach (Auth::user()->posts as $video)
     @include('posts.show', ['element' => $video])
