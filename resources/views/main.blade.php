82098ccf9dbcffeafb14d4bba413364f4cc098e7
CamFush <cjfrost28@gmail.com>
2018-10-23 10:12:36 +1100

    Removed 'show users timeline'

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,7 +3,6 @@
 
 @section('content')
     <article class="content dashboard-page">
-      Show the user's timeline
       @foreach (Auth::user()->timeline as $post)
         @include('posts.show', ['element' => $post])
       @endforeach
