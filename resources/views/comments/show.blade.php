bdcd8b15502bf4e16017b3c70a898dc49249802f
Joe Gibbs <joe.gibbs0@gmail.com>
2018-10-23 10:14:12 +1100

    Changed showing comment time to be readable

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -1,4 +1,4 @@
 <tr class="row">
-  <td class="col-lg-4 reduce-size">{{$element->user->name}} {{$element->updated_at->format('d/m/y')}} {{$element->updated_at->format('H:i')}}:</td>
+  <td class="col-lg-4 reduce-size">{{$element->user->name}} {{\Carbon\Carbon::createFromTimeStamp(strtotime($element->created_at))->diffForHumans()}}:</td>
   <td class="col-lg-8 reduce-size-slightly">{{$element->content}}</td>
 </tr>
