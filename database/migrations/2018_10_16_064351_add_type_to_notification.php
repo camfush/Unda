cbe4e8ad0208d8e584f39df976e57e23c248933c
CamFush <cjfrost28@gmail.com>
2018-10-28 10:31:37 +1100

    Reset everything

--- database/migrations/2018_10_16_064351_add_type_to_notification.php
+++ database/migrations/2018_10_16_064351_add_type_to_notification.php
@@ -25,7 +25,7 @@ class AddTypeToNotification extends Migration
      */
     public function down()
     {
-      Schema::table('notifications', function (Blueprint $table) {
+      Schema::table('posts', function (Blueprint $table) {
         $table->dropIfExists('notification_type');
       });
     }
